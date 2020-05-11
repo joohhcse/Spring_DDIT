550dbef06550933a08592920669533f735f7d857
hh.joo <joohhcse@gmail.com>
2020-05-11 12:36:38 +0900

    spring_repository

---
+++ Spring_Repository/src/test/java/com/spring/test/TestMemberDAOImpl.java
@@ -0,0 +1,54 @@
+package com.spring.test;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.dao.MemberDAO;
+import com.spring.dto.MemberVO;
+
+import junit.framework.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/com/spring/context/root-context.xml")
+@TransactionConfiguration(transactionManager="transactionManager")
+@Transactional
+public class TestMemberDAOImpl {
+
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	
+	@Test
+	public void testSelectMember() throws SQLException {
+		String id = "mimi";
+		
+		MemberVO member = memberDAO.selectMemberById(id);
+		
+		Assert.assertEquals(id, member.getId());
+	}
+	
+	@Test
+	public void testInsertMember()throws SQLException{
+		MemberVO member = new MemberVO();
+		member.setId("kaka");
+		member.setPwd("kaka");
+		member.setName("kaka");
+		member.setEmail("kaka@kaka.net");
+		member.setPhone("000-0000-0000");
+		member.setPicture("noImage.jpg");
+		
+		memberDAO.insertMember(member);
+		
+		MemberVO result = memberDAO.selectMemberById("kaka");
+		
+		Assert.assertEquals(member.getId(),result.getId());
+	}
+	
+}
